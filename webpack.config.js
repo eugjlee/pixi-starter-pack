fac3366075bfbfd2cddedffff863ef42a0a30687
hendrikniemann <h.neverman@web.de>
2018-03-24 22:11:51 +0100

    Upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,41 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, 'app'),
-  entry: path.join(__dirname, 'src', 'index.js'),
-  devtool: 'inline-source-map',
+  mode: 'development',
   output: {
     path: path.join(__dirname, 'bin'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
     alias: {
-      pixi: 'pixi.js'
-    }
+      pixi: 'pixi.js',
+    },
   },
   node: {
-    fs: 'empty'
+    fs: 'empty',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader"
-      }, {
+        use: 'babel-loader',
+      },
+      {
         test: /\.jpe?g$|\.svg$|\.png$|\.wav$|\.mp3$/,
         exclude: /node_modules/,
-        use: "file-loader?name=[path][name].[ext]"
-      }, {
+        use: 'file-loader?name=[path][name].[ext]',
+      },
+      {
         test: /index.html$/,
         exclude: /node_modules/,
-        use: "file-loader?name=index.html"
-      }, {
-        test: /\.json$/,
-        exclude: /node_modules/,
-        use: "json-loader"
-      }, {
-        test: /\.json$/,
-        include: path.join(__dirname, 'node_modules', 'pixi.js'),
-        use: 'json-loader'
+        use: 'file-loader?name=index.html',
       },
       {
         include: path.resolve(__dirname, 'node_modules/pixi.js'),
         use: 'transform-loader?brfs',
-        enforce: 'post'
-      }
+        enforce: 'post',
+      },
     ],
-  }
+  },
 };
