0b04f5993e8654a400cf5c5f61a27f9b7d6acb90
hendrikniemann <h.neverman@web.de>
2018-03-25 01:36:06 +0100

    Add prettier dependency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import './index.html';
 import * as PIXI from 'pixi.js';
 
-const renderer = PIXI.autoDetectRenderer(400, 800, { backgroundColor: 0x1099bb });
+const renderer = PIXI.autoDetectRenderer(400, 800, {
+  backgroundColor: 0x1099bb
+});
 
 if (document.body) {
   document.body.appendChild(renderer.view);
